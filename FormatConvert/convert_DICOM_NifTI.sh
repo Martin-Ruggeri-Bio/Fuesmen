2a75f8eadb3106c6b605be05a24c26d48e45af79
RD Body <daniel@Fuesmen-dei.fuesmen.edu.ar>
2023-10-06 14:40:12 -0300

    codigo de cambio de formato y extraccion de cerebro funcionando

---
+++ FormatConvert/convert_DICOM_NifTI.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Verificar que se proporcionó un directorio como argumento
+if [ $# -ne 1 ]; then
+  echo "Uso: $0 <directorio>"
+  exit 1
+fi
+
+# Obtener el directorio de origen a partir del argumento
+origin_path="$1"
+
+# Iterar a través de las carpetas en el directorio de origen
+for dir in "$origin_path"/*/; do
+  # Obtener el nombre de la carpeta actual (último elemento del camino)
+  dir_name=$(basename "$dir")
+
+  # Construir el camino final
+  final_path="$origin_path/$dir_name"
+
+  # Ejecutar el comando dcm2niix para convertir las imágenes
+  dcm2niix -f "$dir_name" -p y -z y -o "$final_path" "$final_path"
+done
+
+echo "Proceso completado."
